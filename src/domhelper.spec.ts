89db895523e2e604153e2b8e19f3e0bfae4d51c3
hxsggsz <vt.hugo.2021@gmail.com>
2023-11-17 11:32:42 -0300

    test: add tests for getResults() and getAllResults()

--- src/domhelper.spec.ts
+++ src/domhelper.spec.ts
@@ -1,3 +1,45 @@
-test("should pass without problems", () => {
-  expect(true).toBeTruthy();
+import { Select } from "./domhelper";
+
+let uniqueQuerySelectorMock: jest.SpyInstance<
+  Element | null,
+  [selectors: string],
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any
+>;
+
+let multipleQuerySelectorMock: jest.SpyInstance<
+  NodeListOf<Element>,
+  [selectors: string],
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any
+>;
+
+describe("new Select()", () => {
+  beforeEach(() => {
+    uniqueQuerySelectorMock = jest.spyOn(document, "querySelector");
+    multipleQuerySelectorMock = jest.spyOn(document, "querySelectorAll");
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('when call "getResult()"', () => {
+    test("calls the querySelector function", () => {
+      new Select<HTMLDivElement>().where("class").equals("jest").getResult();
+
+      expect(uniqueQuerySelectorMock).toHaveBeenCalled();
+    });
+  });
+
+  describe('when call "getAllResults()"', () => {
+    test("calls the querySelector function", () => {
+      new Select<HTMLDivElement>()
+        .where("class")
+        .equals("jest")
+        .getAllResults();
+
+      expect(multipleQuerySelectorMock).toHaveBeenCalled();
+    });
+  });
 });
